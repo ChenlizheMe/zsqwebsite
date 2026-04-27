f292a8c8cf5bcc44c6e53ceb70ddde6277bcd30e
LizheChen <574118090@qq.com>
2026-04-27 20:59:52 +0800

    remove repoBase url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages serves at https://<user>.github.io/<repo>/
-// If you rename the repository, update the path below to match.
-const repoBase = "/zsqwebsite/";
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.CI === "true" ? repoBase : "/",
+  base: "/",  // 改成这样，不是用 repoBase 了
 });
